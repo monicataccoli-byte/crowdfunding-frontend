37e0cf669162533dd3db6629a74fa8c538e76866
monicataccoli <monicataccoli@gmail.com>
2026-04-07 13:42:19 +1000

    modified package lock Json, package json, src/NavBar.jsx and NavBar.css modified to add heart logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,27 +1,31 @@
-import { Link, Outlet } from "react-router-dom"; 
-import "./NavBar.css"; 
+import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
+import './NavBar.css';
 
-function NavBar() {
+const NavBar = () => {
     return (
         <>
             <nav className="navbar">
                 <Link to="/" className="nav-logo">
-                    <span className="logo-icon">🐾</span>
-                    <div className="logo-text-wrapper">
-                        <span className="logo-main">Pawse</span>
-                        <span className="logo-accent">Furever</span>
+                    <div className="logo-container">
+                        <span className="logo-icon">🐾</span>
+                        <div className="logo-text">
+                            <span className="logo-main">Pawse</span>
+                            <span className="logo-accent">Furever</span>
+                        </div>
                     </div>
                 </Link>
+
                 <div className="nav-links">
                     <Link to="/">Home</Link>
                     <Link to="/login" className="nav-login-btn">Login</Link>
                 </div>
             </nav>
-            
-            {/* The Outlet renders the HomePage content below the Nav */}
-            <Outlet /> 
+            <main>
+                <Outlet />
+            </main>
         </>
     );
-}
+};
 
 export default NavBar;
